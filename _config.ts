e7b623bfb4d25e28b05755474933d72c5373cbcc
heatray <heatrayws@gmail.com>
2025-10-12 23:05:02 +0300

    Add action

--- _config.ts
+++ _config.ts
@@ -1,8 +1,25 @@
 import lume from "lume/mod.ts";
 
-const site = lume();
+const site = lume({
+  location: new URL("https://heatray.github.io/racenet/"),
+});
 
 site.copy("_static", ".");
+
+site.ignore(
+  "racenetlegacy.codemasters.com/Content/Showdown/Flash/careerPage1/discipline_results.swf",
+  "racenetlegacy.codemasters.com/Content/Showdown/Flash/careerPage1/extra_info.swf",
+  "racenetlegacy.codemasters.com/Content/Showdown/Flash/careerPage1/high_scores.swf",
+  "racenetlegacy.codemasters.com/Content/Showdown/Flash/careerPage1/recent_results.swf",
+  "racenetlegacy.codemasters.com/Content/Showdown/Flash/careerPage1/results_overview.swf",
+  "racenetlegacy.codemasters.com/Content/Showdown/Flash/joyride/joyride.swf",
+  "racenetlegacy.codemasters.com/Content/css/Compatibility.css",
+  "racenetlegacy.codemasters.com/Content/css/layout-single.css",
+  "racenetlegacy.codemasters.com/Content/scripts/CareerOverview.js",
+  "racenetlegacy.codemasters.com/Content/scripts/TripleBarCarStatsScript.js",
+  "racenetlegacy.codemasters.com/Content/scripts/joyride.js"
+);
+
 site.copy("racenetlegacy.codemasters.com", ".");
 
 export default site;
