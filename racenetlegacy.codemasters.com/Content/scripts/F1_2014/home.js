d5ba347db7c92b0d933de96330d76229a57f0c5c
heatray <heatrayws@gmail.com>
2025-11-16 16:15:11 +0300

    Add other files

---
+++ racenetlegacy.codemasters.com/Content/scripts/F1_2014/home.js
@@ -0,0 +1,326 @@
+﻿/*global $, angular, lineGraph */
+
+// Chart
+var settings = {
+    canvasId: '#graph-canvas',
+
+    width: 726,
+    height: 240,
+
+    dataUrl: window.graphUrl,
+
+    axisOffsetX: 1,
+    axisOffsetY: 30,
+
+    axisColour: "#fff",
+    axisLineWidth: 2,
+    axisWatermarkWidth: 1,
+    axisWatermarkAlpha: 0.3,
+    numWatermarks: 5,
+
+    numHorizontalSegments: 8,
+
+    pointLineColour: "rgba(255, 255, 255, 0.75);",
+    pointLineWidth: 4,
+
+    areaFillColour: "rgba(0, 43, 61, 0.5);",
+    areaFillAlpha: 0.25,
+
+    axisYContainer: '#graph-axis-y',
+    axisXContainer: '#graph-axis-x',
+    axisLabelTemplateY: '#graph-axis-marker-template-y',
+    axisLabelTemplateX: '#graph-axis-marker-template-x',
+    axisLabelTemplateText: '.graph-axis-label-text',
+
+    axisYRoundToNearestX: 10,
+    axisYLabelOffsetX: 7,
+
+    axisXRoundToNearestX: 10,
+    axisXLabelOffsetY: 14,
+
+    numberGroupSeparator: window.numberGroupSeparator,
+    timeFormatSeparator: ':'
+};
+
+var graph = new lineGraph(window.settings);
+
+function showTooltip(show, time, frequency, location) {
+    "use strict";
+
+    var tooltip = $('.graph-tooltip');
+
+    if (show) {
+        tooltip.show();
+        tooltip.find('#graph-tooltip-frequency').text(frequency);
+        tooltip.find('#graph-tooltip-time').text(time);
+        tooltip.css('left', location.x - tooltip.width() / 2);
+        tooltip.css('top', location.y - (tooltip.height() + 10));
+    } else {
+        tooltip.hide();
+    }
+}
+
+function addPointMarker(graphContainerId, markerCssClassId, graph, modelData, dataElement) {
+    "use strict";
+
+    var d, point, marker, width, height;
+
+    // Lookup graph element in model data that corresponds to player data
+    for (d = 0; d < modelData.GraphElements.length; ++d) {
+        if (modelData.GraphElements[d].X === dataElement.Time && dataElement.Time > 0) {
+            point = graph.plotDataPoint(modelData.AxisX, modelData.AxisY, modelData.GraphElements[d]);
+            marker = $(document.createElement('div'));
+
+            if (dataElement.DisplayTime !== undefined) {
+                dataElement.Time = dataElement.DisplayTime;
+            } else {
+                dataElement.Time = graph.convertLongToTimeString(dataElement.Time);
+            }
+
+            marker.attr('class', markerCssClassId);
+
+            marker.html('&nbsp;');
+
+            width = marker.width();
+            height = marker.height();
+
+            if (width === 0) {
+                width = 20;
+            }
+            if (height === 0) {
+                height = 20;
+            }
+
+            marker.css('left', (point.x - width / 2));
+            marker.css('top', (point.y - height / 2));
+
+            marker.mouseenter(function () {
+                showTooltip(true, dataElement.Time, graph.convertToSeperatedNumber(dataElement.Frequency, settings.numberGroupSeparator), point);
+            });
+
+            marker.mouseleave(function () {
+                showTooltip(false, null, null, null);
+            });
+
+            marker.appendTo(graphContainerId);
+            break;
+        }
+    }
+}
+
+function updateRecordWidget(elementName, data) {
+    "use strict";
+
+    var recordWidget = $(elementName);
+    if (data.DisplayName != null || data.DisplayName !== '-') {
+        recordWidget.find('.ir-name').text(data.DisplayName);
+    }
+    else {
+        recordWidget.find('.ir-name').text('-');
+    }
+    if (data.Time > 0) {
+        recordWidget.find('.ir-time').text(data.DisplayTime);
+    }
+    else {
+        recordWidget.find('.ir-time').text('');
+    }
+}
+
+function processGraphData(data) {
+    "use strict";
+
+    var i;
+
+    // Check for no data state
+    if (data.WorldBestData.Time === 0) {
+        $('#graph_no_data').show();
+    }
+
+    // Update individual record widgets              
+    updateRecordWidget('#ir-player', data.PlayerData);
+    updateRecordWidget('#ir-friend', data.FriendData);
+    updateRecordWidget('#ir-world', data.WorldBestData);
+
+    // Lookup graph element in model data that corresponds to player data
+    addPointMarker('#graph-container', 'graph-point-marker-player', graph, data, data.PlayerData);
+
+    // Lookup graph element in model data that corresponds to player data
+    addPointMarker('#graph-container', 'graph-point-marker-friend', graph, data, data.FriendData);
+
+    addPointMarker('#graph-container', 'graph-point-marker-best', graph, data, data.WorldBestData);
+
+    for (i = 0; i < data.PointElements.length; ++i) {
+        addPointMarker('#graph-container', 'graph-point-marker-data', graph, data, data.PointElements[i]);
+    }
+}
+
+$(document).ready(function () {
+    "use strict";
+
+    var level = window.level,
+        levelEnd,
+        gameLevel,
+        percentage = (window.xp / levelEnd) * 100 + "%",
+        pointsRemaining = level - window.xp;
+
+    $('#right').css({
+        'background-attachment': 'local'
+    });
+
+    $('#scoresMobile').flexslider({
+        // touch: true,
+        directionNav: false,
+        animation: "slide",
+        controlNav: true
+    });
+
+    switch (level) {
+        case "0":
+            levelEnd = 3100;
+            gameLevel = 1;
+            break;
+
+        case "1":
+            levelEnd = 10100;
+            gameLevel = 2;
+            break;
+
+        case "2":
+            levelEnd = 22700;
+            gameLevel = 3;
+            break;
+
+        case "3":
+            levelEnd = 44500;
+            gameLevel = 4;
+            break;
+
+        case "4":
+            levelEnd = 80700;
+            gameLevel = 5;
+            break;
+
+        case "5":
+            levelEnd = 134800;
+            gameLevel = 6;
+            break;
+
+        case "6":
+            levelEnd = 196200;
+            gameLevel = 7;
+            break;
+
+        case "7":
+            levelEnd = 266100;
+            gameLevel = 8;
+            break;
+
+        default:
+            levelEnd = 3100;
+            gameLevel = 1;
+            break;
+    }
+
+    if (window.xp >= 266100) {
+        percentage = "100%";
+        $('#xpAmount').stop(5000).animate({
+            "width": percentage
+        }, 2500);
+        $("#nextUnlock").hide();
+        $("#nextXpLevelBg p").hide();
+    } else {
+        $('#xpAmount').stop(5000).animate({
+            "width": percentage
+        }, 2500);
+        $('.helmets').html('<p>' + gameLevel + '/8</p>');
+        $("#nextUnlock").show();
+        $("#nextXpLevelBg p").show();
+    }
+
+    $('#track_select').dropkick({
+        theme: 'f12014'
+    });
+
+    $('#graph').bind('LineGraphDrawn', function () {
+        var scope = angular.element(document.getElementById('graph')).scope();
+
+        scope.stopLoading();
+        scope.$apply();
+    });
+});
+
+var homeApp = angular.module('homeApp', []);
+
+homeApp.controller('GraphCtrl', function ($scope, $window) {
+    "use strict";
+
+    $scope.track = $window.defaultTrackId;
+    $scope.zoom = "all";
+    $scope.wet = false;
+    $scope.assists = true;
+
+    $scope.$watchCollection('[track, zoom, wet, assists]', function () {
+        $scope.get();
+    });
+
+    $scope.stopLoading = function () {
+        $scope.loading = false;
+    };
+
+    $scope.get = function () {
+        $scope.loading = true;
+
+        var timeSlice = 0,
+            cv,
+            ctx,
+            viewModel;
+
+        switch ($scope.zoom) {
+            case "5sec":
+                timeSlice = 5000;
+                break;
+            case "2sec":
+                timeSlice = 2000;
+                break;
+            default:
+                timeSlice = 60000;
+        }
+
+        viewModel = {
+            "GraphRequest": {
+                "Mode": 3,
+                "Track": $scope.track,
+                "WeatherWet": $scope.wet,
+                "AssistsOn": $scope.assists,
+                "TimeSlice": timeSlice
+            }
+        };
+
+        if (window.graph !== null) {
+            // Reset canvas
+            cv = document.getElementById('graph-canvas');
+            ctx = cv.getContext('2d');
+
+            ctx.save();
+
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+            ctx.clearRect(0, 0, cv.width, cv.height);
+
+            ctx.restore();
+
+            // Hide no data message
+            $('#graph_no_data').hide();
+
+            // Remove event bindings
+            $('.graph-point-marker-player, .graph-point-marker-friend, .graph-point-marker-best, .graph-point-marker-data').unbind();
+
+            // Remove axis labels
+            $('#graph').find('.graph-axis-marker-x, .graph-axis-marker-y').remove();
+
+            // Remove data markers
+            $('.graph-point-marker-player, .graph-point-marker-friend, .graph-point-marker-best, .graph-point-marker-data').remove();
+
+            window.graph.getGraphData(viewModel.GraphRequest, window.processGraphData);
+        }
+    };
+});
---
+++ racenetlegacy.codemasters.com/Content/scripts/line-graph.js
@@ -0,0 +1,275 @@
+﻿function lineGraph(settings)
+{
+    this.graphSettings = settings;
+}
+
+lineGraph.prototype = {
+    convertLongToTimeString: function (value) {
+        var mins = Math.floor(value / 60000);
+        var secs = Math.floor((value % 60000) / 1000);
+        var ms = Math.floor((value % 60000) % 1000);
+
+        var s = "";
+        if (mins < 10) s += "0";
+        s += mins + this.graphSettings.timeFormatSeparator;
+        if (secs < 10) s += "0";
+        s += secs + ".";
+        if (ms < 10) s += "0";
+        s += ms;
+        // Stylistic choice: Only show two digits for milliseconds
+        if (ms > 99) s = s.slice(0, s.length - 1);
+
+        return s;
+    },
+
+    convertToSeperatedNumber: function (nStr, delimitingCharacter) {
+        nStr += '';
+        x = nStr.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + delimitingCharacter + '$2');
+        }
+        return x1 + x2;
+    },
+
+    getWatermarkSeparation: function () {
+        return (this.graphSettings.height - this.graphSettings.axisOffsetY) / this.graphSettings.numWatermarks;
+    },
+
+    getWatermarkYCoordinateFor: function (index) {
+        var watermarkSpacing = this.getWatermarkSeparation();
+        return this.graphSettings.height - index * watermarkSpacing - watermarkSpacing;
+    },
+
+    roundUpToNearestX: function (value, boundary) {
+        return Math.ceil(value / boundary) * boundary;
+    },
+
+    roundDownToNearestX: function (value, boundary) {
+        return Math.floor(value / boundary) * boundary;
+    },
+
+    drawGraphOutline: function (context) {
+        context.save();
+
+        // Setup line stroke style 
+        context.strokeStyle = this.graphSettings.axisColour;
+        context.lineWidth = this.graphSettings.axisLineWidth;
+
+        // Draw vertical axis 
+        context.beginPath();
+        context.moveTo(this.graphSettings.axisOffsetX, this.graphSettings.height);
+        context.lineTo(this.graphSettings.axisOffsetX, 0);
+        context.stroke();
+
+        // Draw horizontal axis 
+        context.beginPath();
+        context.moveTo(this.graphSettings.axisOffsetX, this.graphSettings.height);
+        context.lineTo(this.graphSettings.width - this.graphSettings.axisOffsetX, this.graphSettings.height);
+        context.stroke();
+
+        context.restore();
+    },
+
+    drawGraphWatermarks: function (context) {
+        // Draw y-axis watermarks 
+        context.save();
+
+        // Setup stroke style 
+        context.strokeStyle = this.graphSettings.axisColour;
+        context.lineWidth = this.graphSettings.axisWatermarkWidth;
+        context.globalAlpha = this.graphSettings.axisWatermarkAlpha;
+
+        var numWatermarks = this.graphSettings.numWatermarks;
+        var watermarkSpacing = this.getWatermarkSeparation();
+        for (var idx = 0; idx < numWatermarks; ++idx) {
+            // Draw each watermark line as a path 
+            var y = this.getWatermarkYCoordinateFor(idx);
+            context.beginPath();
+            context.moveTo(this.graphSettings.axisOffsetX, y);
+            context.lineTo(this.graphSettings.width, y);
+            context.stroke();
+        }
+
+        context.restore();
+    },
+
+    drawGraphPoints: function (context, pointData) {
+        // Point data is an array of PointData elements, sorted in ascending order 
+        // Fill area under graphed line 
+        context.save();
+        context.fillStyle = this.graphSettings.areaFillColour;
+        context.strokeStyle = this.graphSettings.areaFillColour;
+        context.lineWidth = 0;
+        context.beginPath();
+
+        // Create path that includes bottom-left corner, path through data points, then bottom-right corner 
+        context.moveTo(this.graphSettings.axisOffsetX - 0.5, this.graphSettings.height);
+        context.lineTo(pointData[0].x - 0.5, pointData[0].y);
+
+        // Build path out of lines to remaining data points 
+        for (var i = 1; i < pointData.length; ++i) {
+            context.lineTo(pointData[i].x, pointData[i].y + 0.5);
+        }
+        context.lineTo(pointData[pointData.length - 1].x + 0.5, this.graphSettings.height);
+
+        // Apply area fill         
+        context.globalAlpha = this.graphSettings.areaFillAlpha;
+        context.fill();
+        context.restore();
+
+        // Draw path through data points 
+        context.save();
+        context.strokeStyle = this.graphSettings.pointLineColour;
+        context.lineWidth = this.graphSettings.pointLineWidth;
+        context.beginPath();
+
+        // Move to location of first data point 
+        context.moveTo(pointData[0].x, pointData[0].y);
+
+        // Build path out of lines to remaining data points 
+        for (var i = 1; i < pointData.length; ++i) {
+            context.lineTo(pointData[i].x, pointData[i].y);
+        }
+        context.stroke();
+        context.restore();
+    },
+
+    drawGraph: function (pointData) {
+        var canvas = $(this.graphSettings.canvasId)[0];
+        if (canvas.getContext) {
+            // HTML5 canvas supported 
+            var context = canvas.getContext('2d');
+
+            // Draw the graph watermarks 
+            this.drawGraphWatermarks(context);
+
+            // Plot the data 
+            this.drawGraphPoints(context, pointData);
+
+            // Redraw the axis 
+            this.drawGraphOutline(context);
+
+            $(this.graphSettings.canvasId).trigger("LineGraphDrawn");
+        }
+        else {
+            // No canvas support 
+        }
+    },
+
+    plotDataPoint: function (axisXData, axisYData, dataElement) {
+        var xRange = axisXData.Max - axisXData.Min;
+        var yMax = this.roundUpToNearestX(axisYData.Max, this.graphSettings.axisYRoundToNearestX);
+
+        var point = {
+            x: this.graphSettings.axisOffsetX + ((dataElement.X - axisXData.Min) / xRange) * this.graphSettings.width,
+            y: this.graphSettings.height - (this.graphSettings.height - this.graphSettings.axisOffsetY) * dataElement.Y / yMax
+        };
+
+        return point;
+    },
+
+    convertDataModelToGraphModel: function (dataModel) {
+        var pointData = new Array();
+
+        for (var idx = 0; idx < dataModel.GraphElements.length; ++idx) {
+            var element = dataModel.GraphElements[idx];
+            var point = this.plotDataPoint(dataModel.AxisX, dataModel.AxisY, element);
+            pointData.push(point);
+        }
+
+        return pointData;
+    },
+
+    createYAxisLabels: function (data) {
+        var numWatermarks = this.graphSettings.numWatermarks;
+        var watermarkSpacing = this.getWatermarkSeparation();
+
+        // Calculate the display values for the watermark labels 
+        var axisUnits = this.roundUpToNearestX(data.AxisY.Max, this.graphSettings.axisYRoundToNearestX) / numWatermarks;
+
+        // Generate y axis labels anchored to the watermark divisions         
+        for (var idx = 0; idx < numWatermarks; ++idx) {
+            // Draw each watermark line as a path 
+            var y = this.getWatermarkYCoordinateFor(idx);
+            var label = $(this.graphSettings.axisLabelTemplateY).clone();
+            var height = label.height();
+            label.attr('id', 'axis-label-y-' + idx);
+            label.css('position', 'absolute');
+            label.css('display', 'block');
+            label.css('top', y - height / 2);
+            label.css('left', this.graphSettings.axisYLabelOffsetX);
+
+            // Note: $().appendTo() adds these divs in reverse order 
+            var labelValue = Math.round((idx + 1) * axisUnits);
+
+            label.find('div' + this.graphSettings.axisLabelTemplateText).text(this.convertToSeperatedNumber(labelValue, this.graphSettings.numberGroupSeparator));
+            label.appendTo(this.graphSettings.axisYContainer);
+        }
+    },
+
+    createXAxisLabels: function (data) {
+        var numSegments = this.graphSettings.numHorizontalSegments;
+        var timeRange = data.AxisX.Max - data.AxisX.Min;
+        var segmentTimeSlice = timeRange / numSegments;
+        var segmentPixelWidth = segmentTimeSlice / timeRange * this.graphSettings.width;
+
+        // Generate x axis labels at uniform intervals         
+        for (var idx = 0; idx < numSegments; ++idx) {
+            // Draw each watermark line as a path 
+            var x = this.graphSettings.axisOffsetX + ((segmentTimeSlice * (idx + 1)) / timeRange) * this.graphSettings.width - segmentPixelWidth / 2;
+            var label = $(this.graphSettings.axisLabelTemplateX).clone();
+            var width = label.width();
+            label.attr('id', 'axis-label-x-' + idx);
+            label.css('position', 'absolute');
+            label.css('text-align', 'left');
+            label.css('display', 'inline');
+            label.css('float', 'left');
+            label.css('top', this.graphSettings.axisXLabelOffsetY);
+            label.css('left', x - width / 2);
+
+            // Note: $().appendTo() adds these divs in reverse order             
+            var labelValue = data.AxisX.Min + (idx + 1) * segmentTimeSlice - segmentTimeSlice / 2;
+
+            label.find('div' + this.graphSettings.axisLabelTemplateText).text(this.convertLongToTimeString(labelValue));
+            label.appendTo(this.graphSettings.axisXContainer);
+        }
+    },
+
+    createAxisLabels: function (data) {
+        // Create axis labels for Y axis 
+        this.createYAxisLabels(data);
+
+        // Create axis labels for X axis 
+        this.createXAxisLabels(data);
+    },
+
+    getGraphData: function (graphRequest, onSuccess) {
+        var self = this;
+        var request = {
+            url: this.graphSettings.dataUrl,
+            type: 'GET',
+            dataType: 'json',
+            data: graphRequest,
+            success: function (data, status, xhr) {
+                // Map model data to graph-friendly data model 
+                var pointData = self.convertDataModelToGraphModel(data);
+
+                // Draw the data onto the graph 
+                self.drawGraph(pointData);
+
+                // Label the axes 
+                self.createAxisLabels(data);
+
+                // Call the supplied onSuccess method
+                onSuccess(data);
+            }
+        };
+
+        $.ajax(request);
+    }
+
+};
+  
